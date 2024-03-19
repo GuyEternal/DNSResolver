41d188cdbfbf6ae5c4285033c07be7fab81e2386
Deep Jawale <99432276+GuyEternal@users.noreply.github.com>
2024-03-20 00:56:29 +0530

    Add serialization implementation + send dns query as a test to 1.1.1.1:53

---
+++ src/main.py
@@ -0,0 +1,28 @@
+from classdefs import *
+from serialize import *
+from sys import getsizeof
+
+def main():
+    msg = Message(
+        Header(
+            id=0x8298, # As long as we're sending only one request it can be any value, only if we are sending multiple requests we need to keep track of the id so that we can match the response with the request
+            flags= QR.QUERY.value | OPCODE.QUERY.value | RD.RECURSION_NOT_DESIRED.value, # We take or of the values to set the flags as 100 | 001 => 101 so we can use the enum values directly and take or of them to set the flags 
+            QDCOUNT=1, 
+            ANCOUNT=0, 
+            NSCOUNT=0, 
+            ARCOUNT=0),
+        Question(
+            QNAME='example.com',
+            QTYPE=Type.A,
+            QCLASS=Class.IN),
+        None, # No Answer field in query
+        None, # No Authority field in query
+        None # No Additional field in query
+    )
+    byte_string_to_send = serialize_query_message(msg)
+    response = send_udp_message(byte_string_to_send, '1.1.1.1', 53)
+    print(getsizeof(response))
+    print(response)
+
+if __name__ == '__main__':
+    main()
